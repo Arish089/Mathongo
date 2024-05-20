ad9be70af260545057e00a15640f97e609cea39c
Arish089 <152006719+Arish089@users.noreply.github.com>
2024-05-20 17:26:28 +0530

    final commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 const app = express()
 require('dotenv').config()
 
-const port = process.env.port
+const port = process.env.Port
 const url = process.env.DB_URL
 
 app.use(express.json())
